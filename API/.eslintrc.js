22dd49d90df05d024d287e25f2f7d8d0fbfa1a68
Borja MPM <bmarquezdelaplata@uc.cl>
2023-09-25 18:08:42 -0300

    linter test

---
+++ API/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    plugins: ["jest"],
+    env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+      "jest/globals": true,
+    },
+    extends: ["airbnb-base", "prettier"],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [".eslintrc.{js,cjs}"],
+        parserOptions: {
+          sourceType: "script",
+        },
+      },
+    ],
+    parserOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {
+      "no-console": "off",
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/*.test.js"],
+        },
+      ],
+    },
+  };
