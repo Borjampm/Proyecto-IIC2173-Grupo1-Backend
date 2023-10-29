cc39148b98804332930ad4d460e0d1d2c951eea0
Alzvil <villena.alan@gmail.com>
2023-10-29 13:15:35 -0300

    changes 2

--- workers/src/load_test/locust.py
+++
@@ -1,7 +0,0 @@
-from locust import HttpUser, task
-from random import randint,seed
-seed(1)
-class SumLoadTest(HttpUser):
-    @task
-    def sum_load_test(self):
-        self.client.post("http://nginx/producer/api/sum", json={"number": randint(1000000, 50000000)})
