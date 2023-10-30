be3cb0f64f551e1b2d6ed3fc3713e9e90478899a
Alzvil <villena.alan@gmail.com>
2023-10-29 23:31:56 -0300

    predictions post

--- API/src/routes.js
+++ API/src/routes.js
@@ -4,6 +4,7 @@ const Companies = require('./routes/company');
 const Users = require('./routes/user');
 const Transactions = require('./routes/transaction');
 const Register = require('./routes/register');
+const Prediction = require('./routes/predictions');
 
 const router = new Router();
 
@@ -12,5 +13,6 @@ router.use('/companies', Companies.routes());
 router.use('/users', Users.routes());
 router.use('/transactions', Transactions.routes());
 router.use('/registers', Register.routes());
+router.use('/predictions', Prediction.routes());
 
 module.exports = router;
