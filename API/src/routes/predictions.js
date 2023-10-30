c9b3526acb3ee203c51c9cc731c4358cfb4c4fab
Borja MPM <bmarquezdelaplata@uc.cl>
2023-10-30 04:01:22 -0300

    cleaned endpoints

--- API/src/routes/predictions.js
+++ API/src/routes/predictions.js
@@ -59,36 +59,23 @@ router.post('predictions.create', '/', async (ctx) => {
 });
 
 
-router.post('predictions.new', '/new', async (ctx) => {
+router.get('predictions.new', '/view_result/:job_id', async (ctx) => {
     console.log("NEWW")
     try {
 
-        console.log("[API] Starting new POST /predictions/")
-
-        const body = ctx.request.body;
-
-        const days_back = body.DaysBack;
-        const symbol = body.Symbol;
-        const quantity = body.Quantity;
-        const id_user = body.Id;
-
-        const fechaActual = new Date();
-        const fechaISO = fechaActual.toISOString();
-
-        let job_id = null;
-        let prediction = null;
-
-        prediction = await ctx.orm.Prediction.create({
-            user_id: id_user,
-            job_id: job_id,
-            state: unfinished,
-            value: null,
-            days_back: days_back,
-            symbol: symbol,
-            quantity: quantity,
-            datetiem: fechaISO
+        const job_id = ctx.params.job_id;
+        axios.get(`${WORKERS_URL}job/${job_id}`)
+        .then(async (res) => {
+            console.log('[API] Prediction posted in Workers')
+            console.log(res.data)
+            ctx.body = res.data;
+            ctx.status = 200;
+        })
+        .catch((error) => {
+            console.log("EEEEEE", error)
+            // throw new Error('[API] Error posting prediction in Workers')
+            message = {message: 'error'};
         });
-
         ctx.body = prediction;
         ctx.status = 201;
     } catch (err) {
@@ -96,31 +83,29 @@ router.post('predictions.new', '/new', async (ctx) => {
     }
 });
 
-router.patch('predictions.new', '/edit', async (ctx) => {
-    console.log("NEWW")
+router.get('userpredictions', '/:userid', async (ctx) => {
     try {
+        // Obtener la parte de la fecha de la cadena ISO 8601
 
-        const body = ctx.request.body;
-
-        const pid = body.predictionid;
-        const job_id = body.job_id;
-        const value = body.value;
-
-        const prediction = await ctx.orm.Prediction.findOne({
+        const predictions = await ctx.orm.Prediction.findAll({
             where: {
-                id: pid
+                user_id: ctx.request.params.userid
             }
         });
-        prediction.state = "FINISHED"
-        prediction.value = value
-        prediction.value = value
 
-        ctx.body = prediction;
+
+        ctx.body = {
+            predictions: predictions,
+        };
+        console.error("predictions yei", predictions);
         ctx.status = 200;
-    } catch (err) {
-        console.error(err);
+    } catch (error) {
+        console.error("predictions bu", error);
+        ctx.body = generalError;
+        ctx.status = 400;
     }
 });
+
 
 
 module.exports = router;
