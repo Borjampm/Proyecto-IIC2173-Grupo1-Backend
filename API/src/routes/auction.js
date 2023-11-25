e5d669b975a495dc799fcde635fd06bf2b12caa4
Borja MPM <bmarquezdelaplata@uc.cl>
2023-11-25 14:14:43 -0300

    implementing auctions endpoints (not finished)

--- API/src/routes/auction.js
+++ API/src/routes/auction.js
@@ -2,8 +2,21 @@ const Router = require('koa-router');
 const { consoleError, generalError } = require('../parameters/errors.js');
 const { defaultPage, defaultSize } = require('../parameters/request.js');
 const { getStartIndex } = require('../utils/request.js');
+const mqtt = require('mqtt');
+const { v4: uuidv4 } = require('uuid');
 
 const router = new Router();
+
+const MQTT_BROKER_URL = 'mqtt://broker.legit.capital';
+const MQTT_PORT = 9000;
+const MQTT_USER = 'students';
+const MQTT_PASSWORD = 'iic2173-2023-2-students';
+
+const client = mqtt.connect(MQTT_BROKER_URL, {
+    port: MQTT_PORT,
+    username: MQTT_USER,
+    password: MQTT_PASSWORD,
+    });
 
 router.get('auctions.show', '/all', async (ctx) => {
     try {
@@ -29,7 +42,49 @@ router.post('auction.create', '/new', async (ctx) => {
             group_id: request.group_id,
             type: request.type
         });
-        console.log('[API] Auction logged', ctx.body)
+        console.log('[API] Auction logged')
+        ctx.status = 201;
+    } catch (error) {
+        console.error(consoleError, error);
+        ctx.body = generalError;
+        ctx.status = 400;
+    }
+});
+
+router.post('auction.propose', '/offer', async (ctx) => {
+    try {
+        const message =  {
+            "auction_id": uuidv4(),
+            "proposal_id": "",
+            "stock_id": uuidv4(),
+            "quantity": 2,
+            "group_id": 1,
+            "type": "offer"
+        };
+        const payload = JSON.stringify(message);
+        client.publish('stocks/auctions', payload);
+        ctx.body = message;
+        ctx.status = 201;
+    } catch (error) {
+        console.error(consoleError, error);
+        ctx.body = generalError;
+        ctx.status = 400;
+    }
+});
+
+router.get('auction.propose', '/propose', async (ctx) => {
+    try {
+        const message =  {
+            "auction_id": 1234,
+            "proposal_id": "",
+            "stock_id": 1234,
+            "quantity": 2,
+            "group_id": 1,
+            "type": "proposal"
+        };
+        const payload = JSON.stringify(message);
+        client.publish('stocks/auctions', payload);
+        ctx.body = message;
         ctx.status = 201;
     } catch (error) {
         console.error(consoleError, error);
