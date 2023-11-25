c46557b369c53315261c95ddd8efccc2baa91290
Alzvil <aqguzman@uc.cl>
2023-11-25 15:20:33 -0300

    fix linter routes/avaliblestocks.js

--- API/src/routes/availablestocks.js
+++ API/src/routes/availablestocks.js
@@ -1,15 +1,14 @@
 const Router = require('koa-router');
-const { Op } = require('sequelize');
+// const { Op } = require('sequelize');
 const { consoleError, generalError } = require('../parameters/errors.js');
-const { defaultPage, defaultSize } = require('../parameters/request.js');
-const { getStartIndex } = require('../utils/request.js');
+// const { defaultPage, defaultSize } = require('../parameters/request.js');
+// const { getStartIndex } = require('../utils/request.js');
 
 const router = new Router();
 
 router.get('stock.available', '/all', async (ctx) => {
     try {
-
-        }
+        ctx.body = 'Implementación Pendiente'
         ctx.status = 201;
     } catch (error) {
         console.error(consoleError, error);
